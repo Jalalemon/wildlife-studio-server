435b72f4eb632305af40f4bd911d48cee0d74ef0
Your Name <ju679935@gmail.com>
2022-11-08 17:03:45 +0600

    reviews get

--- index.js
+++ index.js
@@ -24,12 +24,42 @@ async function run(){
         const serviceCollection = client
           .db("wildlife-studio")
           .collection("reviews");
+
+         const reviewsCollection = client.db("wildlife-studio").collection('addReviews'); 
+
+
          app.get("/services", async (req, res) => {
            const query = {};
            const cursor = serviceCollection.find(query);
            const services = await cursor.limit(3).toArray();
            res.send(services);
          });
+
+         app.post('/allReviews', async(req, res) =>{
+            const allReview = req.body;
+            const result = await reviewsCollection.insertOne(allReview);
+            console.log(result);
+            res.send(result);
+         });
+
+         app.get("/allReviews", async (req, res) => {
+        //    const decoded = req.decoded;
+        //    console.log("inside orderapi", decoded);
+        //    if (decoded.email !== req.query.email) {
+        //      res.status(403).send({ message: "unauthorized access" });
+        //    }
+           let query = {};
+           if (req.query.email) {
+             query = {
+               email: req.query.email,
+             };
+           }
+           const cursor = reviewsCollection.find(query);
+           const allreviews = await cursor.toArray();
+           res.send(allreviews);
+         });
+
+
          app.get("/allServices", async (req, res) => {
            const query = {};
            const cursor = serviceCollection.find(query);
